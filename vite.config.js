9c32f2f190c213857a528bd2542b5d4c6bf5999b
BeanNorelle <beansobre@gmail.com>
2024-07-21 13:02:16 +0800

    fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/e-plantShopping/', // Use the repository name
+  base: '/e-plantShopping/', // Base path for GitHub Pages
+  build: {
+    outDir: 'dist', // Output directory
+  },
   plugins: [react()],
 })
